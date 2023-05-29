988003f92e25f409de3faab4830812b5b7e5b77f
vimalct20 <vimal.ct20@bitsathy.ac.in>
2023-05-29 09:18:25 +0530

    Solution for POTDGFG on 29/05/2023

---
+++ CamelCase Pattern Matching.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<string> CamelCase(int N, vector<string> Dictionary, string Pattern) {
+     vector<string>  ans;
+     int n=Pattern.size();
+    for(auto current:Dictionary){
+      
+      
+        string str="";
+        for(auto i:current){
+          
+            if(isupper(i)){
+           
+            str+=i;
+            }
+        }
+          int flag=1;
+            if(str.size()<n) continue;
+            for(int j=0;j<n;j++){
+                if(Pattern[j]!=str[j]){
+                  flag=0;
+                  break;
+                }
+            }
+            
+            if(flag) ans.push_back(current);
+    }
+      if(ans.size()==0) ans.push_back("-1");
+      return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> Dictionary(N);
+        for (int i = 0; i < N; i++) cin >> Dictionary[i];
+        string Pattern;
+        cin >> Pattern;
+        Solution ob;
+        vector<string> ans = ob.CamelCase(N, Dictionary, Pattern);
+        sort(ans.begin(), ans.end());
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
