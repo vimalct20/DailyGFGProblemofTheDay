c89fc9f4dc11b9596f9e70a64564a8b61fa3bba9
vimalct20 <vimal.ct20@bitsathy.ac.in>
2023-06-12 22:31:31 +0530

    Solution for POTDGFG on 12/06/2023

---
+++ Rod Cutting.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            int x=0,y=0;
+            
+            if(j-i>-1){
+                x= price[i-1]+dp[i][j-i];
+            }
+            y=dp[i-1][j];
+            dp[i][j]=max(x,y);
+        }
+    }
+    return dp[n][n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
