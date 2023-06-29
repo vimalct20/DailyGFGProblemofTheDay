947790d9367bf1ae3f7443161b7400517fd48040
Vimal A K <104977324+vimalct20@users.noreply.github.com>
2023-06-29 11:44:47 +0530

    Solution for POTDGFG on 11/06/2023

---
+++ Next Happy Number.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    bool isHappy(int N)
+    {
+        int sum = N, curr = N;
+
+        while (sum > 9)
+        {
+            sum = 0;
+            while (curr > 0)
+            {
+                int d = curr % 10;
+                sum += (d * d);
+                curr = curr / 10;
+            }
+
+            if (sum == 1)
+                return 1;
+            curr = sum;
+        }
+        if (sum == 7)
+            return 1;
+        return 0;
+    }
+    int nextHappy(int N)
+    {
+        int next = N + 1;
+
+        while (1)
+        {
+            if (isHappy(next))
+            {
+                return next;
+            }
+            next++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.nextHappy(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
