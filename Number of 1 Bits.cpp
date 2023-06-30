a3a9354fb028c3e6e79519697c26c3ba56c0b0d9
vimalct20 <vimal.ct20@bitsathy.ac.in>
2023-07-01 01:38:32 +0530

    Solution for POTDGFG on 11/06/2023

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int count=0;
+        while(N>0){
+            int rem=N%2;
+            if(rem==1) count++;
+            N=N/2;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
